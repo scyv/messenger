dab9622608e0c1c4ec421617cc6f3de2876a3533
Yves Schubert <yves.schubert@posteo.de>
2021-07-27 22:51:09 +0200

    add some websocket magic

--- messenger.go
+++ messenger.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"fmt"
 	"net/http"
+	"strings"
 	"time"
 
 	"github.com/gorilla/mux"
@@ -29,23 +30,69 @@ type Message struct {
 	Timestamp time.Time `json:"timestamp"`
 }
 
-func main() {
+type Connection struct {
+	Conn   *websocket.Conn
+	Id     string
+	RoomId string
+}
 
-	var inmemory = make(map[string][]Message)
+func leaveRoom(connId string, roomId string) {
+	idxToKill := -1
+	list, ok := connections[roomId]
+	if !ok {
+		return
+	}
+	for idx, c := range list {
+		if c.Id == connId {
+			idxToKill = idx
+			break
+		}
+	}
+	if idxToKill >= 0 {
+		connections[roomId] = append(list[:idxToKill], list[(idxToKill+1):]...)
+	}
+	fmt.Printf("\nconnection %s left room: %s", connId, roomId)
+}
+
+var inmemory = make(map[string][]Message)
+var connections = make(map[string][]Connection)
+
+func main() {
 
 	r := mux.NewRouter()
 
 	r.HandleFunc("/ws", func(w http.ResponseWriter, r *http.Request) {
 		conn, _ := upgrader.Upgrade(w, r, nil)
+		connect := &Connection{Conn: conn, Id: xid.New().String()}
+		defer func() {
+			leaveRoom(connect.Id, connect.RoomId)
+			connect.Conn.Close()
+			fmt.Printf("\nconnection %s closed", connect.Id)
+		}()
 
 		for {
-			msgType, msg, err := conn.ReadMessage()
+			_, msg, err := conn.ReadMessage()
 			if err != nil {
 				return
 			}
-			fmt.Printf("\n%s sent: %s", conn.RemoteAddr(), string(msg))
-			if err = conn.WriteMessage(msgType, msg); err != nil {
-				return
+			msgAsString := string(msg)
+			var idxEnterRoom = strings.Index(msgAsString, "ENTER ")
+			if idxEnterRoom == 0 {
+				var roomId = msgAsString[6:]
+				connect.RoomId = roomId
+				list, ok := connections[roomId]
+				if ok {
+					connections[roomId] = append(list, *connect)
+				} else {
+					newlist := []Connection{*connect}
+					connections[roomId] = newlist
+				}
+				fmt.Printf("\nconnection %s entered room: %s", connect.Id, connect.RoomId)
+			}
+
+			var idxLeaveRoom = strings.Index(msgAsString, "LEAVE")
+			if idxLeaveRoom == 0 {
+				leaveRoom(connect.Id, connect.RoomId)
 			}
 		}
 	})
@@ -68,6 +115,15 @@ func main() {
 			newlist := []Message{message}
 			inmemory[roomId] = newlist
 		}
+		roomMembers, ok := connections[roomId]
+		if ok {
+			for _, c := range roomMembers {
+				closer, _ := c.Conn.NextWriter(websocket.TextMessage)
+				json.NewEncoder(closer).Encode(message)
+				closer.Close()
+			}
+		}
+
 		fmt.Printf("\nMessage from %s with id %s", message.Sender, message.MessageId)
 	}).Methods("POST")
 
@@ -79,8 +135,7 @@ func main() {
 		if ok {
 			json.NewEncoder(w).Encode(list)
 		} else {
-			w.WriteHeader(http.StatusNotFound)
-			fmt.Fprint(w, "NOT_FOUND")
+			http.Error(w, "Room Not found", http.StatusNotFound)
 		}
 	}).Methods("GET")
 
